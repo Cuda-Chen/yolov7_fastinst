92a7523fcbd5715a29c08d38c3b95d45aa41f29b
Cuda-Chen <clh960524@gmail.com>
2023-11-29 22:45:59 +0800

    Add loss of FastInst Transformer Decoder

--- utils/loss.py
+++ utils/loss.py
@@ -8,6 +8,8 @@ from ..metrics import bbox_iou
 from ..torch_utils import de_parallel
 from .general import crop
 
+from .modeling.criterion import SetCriterion
+from .modeling.matcher import HungarianMatcher
 
 class ComputeLoss:
     # Compute losses
@@ -41,73 +43,57 @@ class ComputeLoss:
         self.anchors = m.anchors
         self.device = device
 
+		# FastInst Transformer decoder
+		# loss weights
+        class_weight = cfg.MODEL.FASTINST.CLASS_WEIGHT
+        dice_weight = cfg.MODEL.FASTINST.DICE_WEIGHT
+        mask_weight = cfg.MODEL.FASTINST.MASK_WEIGHT
+        location_weight = cfg.MODEL.FASTINST.LOCATION_WEIGHT
+        proposal_weight = cfg.MODEL.FASTINST.PROPOSAL_WEIGHT
+
+        # building criterion
+        matcher = HungarianMatcher(
+            cost_class=class_weight,
+            cost_mask=mask_weight,
+            cost_dice=dice_weight,
+            cost_location=location_weight,
+            num_points=cfg.MODEL.FASTINST.TRAIN_NUM_POINTS,
+        )
+
+        weight_dict = {"loss_ce": class_weight, "loss_mask": mask_weight, "loss_dice": dice_weight}
+		weight_dict.update({"loss_proposal": proposal_weight})
+
+        losses = ["labels", "masks"]
+
+        criterion = SetCriterion(
+            sem_seg_head.num_classes,
+            matcher=matcher,
+            weight_dict=weight_dict,
+            eos_coef=no_object_weight,
+            losses=losses,
+            num_points=cfg.MODEL.FASTINST.TRAIN_NUM_POINTS,
+            oversample_ratio=cfg.MODEL.FASTINST.OVERSAMPLE_RATIO,
+            importance_sample_ratio=cfg.MODEL.FASTINST.IMPORTANCE_SAMPLE_RATIO,
+        )
+
     def __call__(self, preds, targets, masks):  # predictions, targets, model
-        p, proto = preds
-        bs, nm, mask_h, mask_w = proto.shape  # batch size, number of masks, mask height, mask width
-        lcls = torch.zeros(1, device=self.device)
-        lbox = torch.zeros(1, device=self.device)
-        lobj = torch.zeros(1, device=self.device)
-        lseg = torch.zeros(1, device=self.device)
-        tcls, tbox, indices, anchors, tidxs, xywhn = self.build_targets(p, targets)  # targets
+       # mask classification target
+		if "instances" in batched_inputs[0]:
+			gt_instances = [x["instances"].to(self.device) for x in batched_inputs] # GT Mask
+			targets = self.prepare_targets(gt_instances, images)
 
-        # Losses
-        for i, pi in enumerate(p):  # layer index, layer predictions
-            b, a, gj, gi = indices[i]  # image, anchor, gridy, gridx
-            tobj = torch.zeros(pi.shape[:4], dtype=pi.dtype, device=self.device)  # target obj
+		outputs = self.sem_seg_head(features, targets)
 
-            n = b.shape[0]  # number of targets
-            if n:
-                pxy, pwh, _, pcls, pmask = pi[b, a, gj, gi].split((2, 2, 1, self.nc, nm), 1)  # subset of predictions
+		# bipartite matching-based loss
+		losses = self.criterion(outputs, targets)
 
-                # Box regression
-                pxy = pxy.sigmoid() * 2 - 0.5
-                pwh = (pwh.sigmoid() * 2) ** 2 * anchors[i]
-                pbox = torch.cat((pxy, pwh), 1)  # predicted box
-                iou = bbox_iou(pbox, tbox[i], CIoU=True).squeeze()  # iou(prediction, target)
-                lbox += (1.0 - iou).mean()  # iou loss
-
-                # Objectness
-                iou = iou.detach().clamp(0).type(tobj.dtype)
-                if self.sort_obj_iou:
-                    j = iou.argsort()
-                    b, a, gj, gi, iou = b[j], a[j], gj[j], gi[j], iou[j]
-                if self.gr < 1:
-                    iou = (1.0 - self.gr) + self.gr * iou
-                tobj[b, a, gj, gi] = iou  # iou ratio
-
-                # Classification
-                if self.nc > 1:  # cls loss (only if multiple classes)
-                    t = torch.full_like(pcls, self.cn, device=self.device)  # targets
-                    t[range(n), tcls[i]] = self.cp
-                    lcls += self.BCEcls(pcls, t)  # BCE
-
-                # Mask regression
-                if tuple(masks.shape[-2:]) != (mask_h, mask_w):  # downsample
-                    masks = F.interpolate(masks[None], (mask_h, mask_w), mode="bilinear", align_corners=False)[0]
-                marea = xywhn[i][:, 2:].prod(1)  # mask width, height normalized
-                mxyxy = xywh2xyxy(xywhn[i] * torch.tensor([mask_w, mask_h, mask_w, mask_h], device=self.device))
-                for bi in b.unique():
-                    j = b == bi  # matching index
-                    if self.overlap:
-                        mask_gti = torch.where(masks[bi][None] == tidxs[i][j].view(-1, 1, 1), 1.0, 0.0)
-                    else:
-                        mask_gti = masks[tidxs[i]][j]
-                    lseg += self.single_mask_loss(mask_gti, pmask[j], proto[bi], mxyxy[j], marea[j])
-
-            obji = self.BCEobj(pi[..., 4], tobj)
-            lobj += obji * self.balance[i]  # obj loss
-            if self.autobalance:
-                self.balance[i] = self.balance[i] * 0.9999 + 0.0001 / obji.detach().item()
-
-        if self.autobalance:
-            self.balance = [x / self.balance[self.ssi] for x in self.balance]
-        lbox *= self.hyp["box"]
-        lobj *= self.hyp["obj"]
-        lcls *= self.hyp["cls"]
-        lseg *= self.hyp["box"] / bs
-
-        loss = lbox + lobj + lcls + lseg
-        return loss * bs, torch.cat((lbox, lseg, lobj, lcls)).detach()
+		for k in list(losses.keys()):
+			if k in self.criterion.weight_dict:
+				losses[k] *= self.criterion.weight_dict[k]
+			else:
+				# remove this loss if not specified in `weight_dict`
+				losses.pop(k)
+		return losses  
 
     def single_mask_loss(self, gt_mask, pred, proto, xyxy, area):
         # Mask loss for one image
